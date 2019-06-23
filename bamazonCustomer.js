39006acf6c091eac69cac314854a8257cee08b97
John Atay <johncanaty@gmail.com>
2019-06-22 17:32:43 -0700

    table on screen

---
+++ bamazonCustomer.js
@@ -0,0 +1,29 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "bamazon_db"
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  afterConnection();
+});
+
+function afterConnection() {
+  connection.query("SELECT * FROM products", (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    connection.end();
+  });
+}
